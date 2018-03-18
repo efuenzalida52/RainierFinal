58dc7b67dba9a9ca7847d40c3dc317a612aef0d5
Sunny Wu <etsunny@gmail.com>
2018-03-18 01:53:07 -0700

    q3 added

--- RF/Controllers/HomeController.cs
+++ RF/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace hw7.Controllers
 
             RainierEntities model = new RainierEntities();
             ViewBag.Message = "Query One: How many of the top 10(score) titles are PC games?";
+            //SELECT TOP 10 IGN_Table.Score, NA_Sales, EU_Sales, Japan_Sales, Other_Sales, Global_Sales FROM IGN_Table JOIN VGSales_Table ON IGN_Table.Title = VGSales_Table.Title WHERE VGSales_Table.Platform = “PC”;
 
             var query1 = (from ign in model.igns
                          join vg in model.vgs
@@ -30,13 +31,43 @@ namespace hw7.Controllers
             return View(IGNList);
         }
 
-        //SELECT TOP 10 IGN_Table.Score, NA_Sales, EU_Sales, Japan_Sales, Other_Sales, Global_Sales FROM IGN_Table JOIN VGSales_Table ON IGN_Table.Title = VGSales_Table.Title WHERE VGSales_Table.Platform = “PC”;
-
-
-       
-        //SELECT TOP 5 IGN_Table.Platform, NA_Sales, EU_Sales, Japan_Sales, Other_Sales, Global_Sales FROM VGSales_Table JOIN IGN_Table ON.VGSales_TableTitle = IGN_Table.Title JOIN SteamSpy_Table ON IGN_Table.title = SteamSpy_Table.Title;
 
         public ActionResult Q2()
+        {
+
+            RainierEntities model = new RainierEntities();
+            ViewBag.Message = "Query Two: What are the platform(s) for top 5 selling games in ea.area?";
+            //SELECT TOP 5 IGN_Table.Platform, NA_Sales, EU_Sales, Japan_Sales, Other_Sales, Global_Sales FROM VGSales_Table JOIN IGN_Table ON.VGSales_TableTitle = IGN_Table.Title JOIN SteamSpy_Table ON IGN_Table.title = SteamSpy_Table.Title;
+
+            var query2 = (from ign in model.igns
+                          join vg in model.vgs on ign.Title equals vg.Title
+                          join steamspy in model.steamspies on vg.Title equals steamspy.Title
+                          select ign).Take(10);
+
+            List<ign> IGNList = query2.ToList();
+
+            return View(IGNList);
+        }
+
+
+        public ActionResult Q3()
+        {
+
+            RainierEntities model = new RainierEntities();
+            ViewBag.Message = "Query Three: What are the top selling game publishers in NA?";
+            //SELECT TOP 3 Publisher, Publisher(s), NA_Sales FROM VGSales_Table JOIN SteamSpy_Table ON VGSales_Table.Title = SteamSpy_Table.Title;
+
+            var query3 = (from vg in model.vgs
+                          join steamspy in model.steamspies on vg.Title equals steamspy.Title
+                          orderby vg.NA_Sales descending
+                          select vg).Take(3);
+
+            List<vg> VGList = query3.ToList();
+
+            return View(VGList);
+        }
+
+        public ActionResult Q4()
         {
 
             RainierEntities model = new RainierEntities();
@@ -51,6 +82,71 @@ namespace hw7.Controllers
 
             return View(IGNList);
         }
+
+        public ActionResult Q5()
+        {
+
+            RainierEntities model = new RainierEntities();
+            ViewBag.Message = "Query Two: What are the platform(s) for top 5 selling games in ea.area?";
+
+            var query2 = (from ign in model.igns
+                          join vg in model.vgs on ign.Title equals vg.Title
+                          join steamspy in model.steamspies on vg.Title equals steamspy.Title
+                          select ign).Take(10);
+
+            List<ign> IGNList = query2.ToList();
+
+            return View(IGNList);
+        }
+
+        public ActionResult Q6()
+        {
+
+            RainierEntities model = new RainierEntities();
+            ViewBag.Message = "Query Two: What are the platform(s) for top 5 selling games in ea.area?";
+
+            var query2 = (from ign in model.igns
+                          join vg in model.vgs on ign.Title equals vg.Title
+                          join steamspy in model.steamspies on vg.Title equals steamspy.Title
+                          select ign).Take(10);
+
+            List<ign> IGNList = query2.ToList();
+
+            return View(IGNList);
+        }
+
+        public ActionResult Q7()
+        {
+
+            RainierEntities model = new RainierEntities();
+            ViewBag.Message = "Query Two: What are the platform(s) for top 5 selling games in ea.area?";
+
+            var query2 = (from ign in model.igns
+                          join vg in model.vgs on ign.Title equals vg.Title
+                          join steamspy in model.steamspies on vg.Title equals steamspy.Title
+                          select ign).Take(10);
+
+            List<ign> IGNList = query2.ToList();
+
+            return View(IGNList);
+        }
+
+        public ActionResult Q8()
+        {
+
+            RainierEntities model = new RainierEntities();
+            ViewBag.Message = "Query Two: What are the platform(s) for top 5 selling games in ea.area?";
+
+            var query2 = (from ign in model.igns
+                          join vg in model.vgs on ign.Title equals vg.Title
+                          join steamspy in model.steamspies on vg.Title equals steamspy.Title
+                          select ign).Take(10);
+
+            List<ign> IGNList = query2.ToList();
+
+            return View(IGNList);
+        }
+
 
         /*       public ActionResult Contact()
                {
